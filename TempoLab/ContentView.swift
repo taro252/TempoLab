3569258737e4245b9335825508307b34a4e662c4
Taro Yoshida <taro252@gmail.com>
2026-09-15 13:47:54 +0900

    Initial Commit

---
+++ TempoLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TempoLab
+//
+//  Created by 吉田 太郎 on 2026/09/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TempoLab/TempoLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempoLabApp.swift
+//  TempoLab
+//
+//  Created by 吉田 太郎 on 2026/09/15.
+//
+
+import SwiftUI
+
+@main
+struct TempoLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
